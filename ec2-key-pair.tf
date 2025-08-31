c7ebd8c0f9d07acd3f9b61c4a2dbd34f1ce68966
shankarduppala <shankarduppala320@gmail.com>
2025-08-31 16:35:49 +0530

    intrigrating key pair into ec2 instance

--- ec2-key-pair.tf
+++ ec2-key-pair.tf
@@ -6,3 +6,9 @@ resource "aws_key_pair" "mykey" {
     key_name   = "MyKey"
     public_key = file("~/.ssh/id_rsa.pub")
 }
+
+resource "aws_instance" "myec2" {
+    ami = "ami-0c02fb55956c7d316"
+    instance_type = "t2.micro"
+    key_name = aws_key_pair.mykey.key_name
+}
