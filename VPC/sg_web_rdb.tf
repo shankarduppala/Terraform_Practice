76a0c9e42a3d213d87afdddcf6e7ee2dadf35ff4
shankarduppala <shankarduppala320@gmail.com>
2025-09-01 16:00:46 +0530

    adding security groups twebserver and rds

---
+++ VPC/sg_web_rdb.tf
@@ -0,0 +1,50 @@
+# Webserver SG
+resource "aws_security_group" "web_sg" {
+  vpc_id = aws_vpc.myvpc.id
+  name   = "web-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "web-sg" }
+}
+
+# RDS SG (only allow from webserver)
+resource "aws_security_group" "rds_sg" {
+  vpc_id = aws_vpc.myvpc.id
+  name   = "rds-sg"
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.web_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "rds-sg" }
+}
